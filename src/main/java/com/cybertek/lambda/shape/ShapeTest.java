8d170f594f216faa3caaa1eba9d8562de10cf763
musaksu <musaksu@gmail.com>
2022-11-17 13:21:06 +0100

    17/11-13.20

--- src/main/java/com/cybertek/lambda/shape/ShapeTest.java
+++ src/main/java/com/cybertek/lambda/shape/ShapeTest.java
@@ -11,5 +11,15 @@ public class ShapeTest {
 
         Drawable3 drawable3 = (shape, name) -> System.out.println(name + " is drawing a " + shape);
         drawable3.draw("Circle","Mike");
+
+        Drawable4 drawable4 = (shape, area) -> "Drawing a " + shape + " and it's area is " + area;
+        System.out.println(drawable4.draw("Circle",200));
+
+        Drawable3 drawable31 = (shape, name) -> {
+            System.out.println("What kind of shape is this" + shape);
+            System.out.println("Who is drawing this shape: " + name);
+
+        };
+        drawable3.draw("Triangle","Mike");
     }
 }
